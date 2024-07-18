32e43a443e7b7daf9d0de47796498930e0363b4d
annu <annupawadia@gmail.com>
2024-07-18 13:34:16 +0530

    added some new files

---
+++ my-react-app/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import cryptocurrency from '../images/cryptocurrency.png';
+import {Stack} from "@mui/material"
+
+const Navbar = () => {
+  return (
+    <Stack>
+      <img src={cryptocurrency.png} alt="crypto"/>
+
+
+
+
+    </Stack>
+  )
+}
+
+export default Navbar
